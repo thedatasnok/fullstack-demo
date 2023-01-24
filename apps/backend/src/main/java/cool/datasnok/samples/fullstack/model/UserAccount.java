3793068ac0821053180b99020ef948a080db7f61
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-01-24 21:17:29 +0100

    fix(backend): set GenerationType for ID column

--- apps/backend/src/main/java/cool/datasnok/samples/fullstack/model/UserAccount.java
+++ apps/backend/src/main/java/cool/datasnok/samples/fullstack/model/UserAccount.java
@@ -2,6 +2,8 @@ package cool.datasnok.samples.fullstack.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -12,6 +14,7 @@ import lombok.Getter;
 public class UserAccount {
   
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "user_account_id")
   private Long id;
 
