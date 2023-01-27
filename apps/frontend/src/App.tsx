baabb842af1c919922c2cc7c6eb56446123cb5e7
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-01-27 23:48:36 +0100

    feat(frontend): add simple sign in form with response preview

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+
 const App = () => {
-  return <div>app?</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [response, setResponse] = useState<Response>();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch('/api/v1/auth/sign-in', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    }).then((res) => setResponse(res));
+  };
+
+  return (
+    <div className='wrapper'>
+      <h1>fullstack-demo-frontend</h1>
+
+      <form onSubmit={handleSubmit} className='form'>
+        <input
+          name='username'
+          placeholder='Username'
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          name='password'
+          placeholder='Password'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>Sign in</button>
+      </form>
+
+      <pre>
+        {response ? JSON.stringify(response.json(), null, 2) : 'no response'}
+      </pre>
+    </div>
+  );
 };
 
 export default App;
