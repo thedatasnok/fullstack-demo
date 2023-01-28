f47a206e2c2055befe79c57d44775b1ea947052d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-01-28 18:21:16 +0100

    fix(infrastructure): set `insecure` flag to true
    this is most likely a bad idea, but I have to test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,12 @@
 provider "kubernetes" {
   config_path = var.kubeconfig_path
+  insecure    = true # This is apparently required for it to work in GitHub Actions?
 }
 
 module "app" {
-  source = "./modules/app"
-  namespace = var.namespace
-  app_version = var.app_version
-  hostname = var.hostname
+  source         = "./modules/app"
+  namespace      = var.namespace
+  app_version    = var.app_version
+  hostname       = var.hostname
   cluster_issuer = var.cluster_issuer
 }
